8187e7957b7fd8cb83f548603929ee423149771a
clarencesarmiento <sarmiento.clarence1020@gmail.com>
2023-10-26 20:05:18 +0800

    Created Backend Directory

---
+++ backend/calculate.py
@@ -0,0 +1,46 @@
+# Constant Variable
+cm_to_m_conversion_factor = 100
+
+
+def calculate_bmi(height_cm: float or int, weight_kg: float or int) -> float:
+    """
+    Function to calculate the Body Mass Index
+
+    :param height_cm: float, height in centimeter.
+    :param weight_kg: float
+    :return: float, calculated Body Mass Index
+    """
+
+    # Check parameter's datatypes
+    if not isinstance(height_cm, (float, int)) or not isinstance(weight_kg, (float, int)):
+        raise TypeError("height_cm and weight_kg must be of type float or int")
+
+    if height_cm <= 0 or weight_kg <= 0:
+        return 0
+
+    # Calculate BMI using the formula: weight / (height^2)
+    bmi = weight_kg / ((height_cm / cm_to_m_conversion_factor) ** 2)
+
+    return bmi
+
+
+def get_bmi_category(bmi: float or int) -> str:
+    """
+    Function to get the Category based on the Body Mass Index.
+
+    :param bmi: float, calculated Body Mass Index
+    :return: str, category based on the calculated Body Mass Index
+    """
+
+    # Check parameter datatype
+    if not isinstance(bmi, (float, int)):
+        raise TypeError('bmi must be of type float or int')
+
+    if bmi < 18.5:
+        return 'Underweight'
+    elif 18.5 <= bmi < 24.9:
+        return 'Normal Weight'
+    elif 25 <= bmi < 29.9:
+        return 'Overweight'
+    else:
+        return 'Obesity'
---
+++ backend/test_calculate.py
@@ -0,0 +1,47 @@
+from backend.calculate import calculate_bmi, get_bmi_category
+import pytest
+
+
+def test_calculate_bmi():
+    assert calculate_bmi(160.0, 54.0) == 21.093749999999996
+
+
+def test_calculate_bmi_zero_and_negative_values():
+    assert calculate_bmi(0.0, 0.0) == 0
+    assert calculate_bmi(-1.0, 0.0) == 0
+    assert calculate_bmi(0.0, -1.0) == 0
+    assert calculate_bmi(150.0, 0.0) == 0
+    assert calculate_bmi(0.0, 150.0) == 0
+
+
+def test_invalid_input_type():
+    with pytest.raises(TypeError):
+        calculate_bmi('160', 54)
+
+    with pytest.raises(TypeError):
+        calculate_bmi(160, '54')
+
+    with pytest.raises(TypeError):
+        calculate_bmi('160', '54')
+
+    with pytest.raises(TypeError):
+        calculate_bmi('a', 55)
+
+    with pytest.raises(TypeError):
+        calculate_bmi(55, 'a')
+
+    with pytest.raises(TypeError):
+        calculate_bmi('a', 'a')
+
+
+def test_category():
+    assert get_bmi_category(16.0) == 'Underweight'
+    assert get_bmi_category(21.1) == 'Normal Weight'
+    assert get_bmi_category(28.0) == 'Overweight'
+    assert get_bmi_category(31.0) == 'Obesity'
+
+
+def test_invalid_category_input():
+    with pytest.raises(TypeError):
+        get_bmi_category('16.0')
+
